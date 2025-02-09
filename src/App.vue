8255a75829d3a7a1adb64585593d37cda11e47d2
Julian Vera <jjveragu@gmail.com>
2025-02-09 19:17:44 -0300

    cambios para build

--- src/App.vue
+++ src/App.vue
@@ -8,20 +8,20 @@ import navComponent from './components/navComponent.vue';
     <div class="railnav w-20 bg-emerald-100 items-center">
       <div class="flex flex-col">
         <span
-          class="flex-col gap-2 flex w-20 h-32 hover:bg-emerald-200 active:bg-emerald-300 items-center justify-center"
+          class="flex-col gap-2 flex w-20 h-20 hover:bg-emerald-200 active:bg-emerald-300 items-center justify-center"
           ><menu-icon :size="32" />
           <span class="text-xs">Agenda</span>
         </span>
         <span
-          class="flex-col gap-2 flex w-20 h-32 hover:bg-emerald-200 active:bg-emerald-300 items-center justify-center"
+          class="flex-col gap-2 flex w-20 h-20 hover:bg-emerald-200 active:bg-emerald-300 items-center justify-center"
           ><menu-icon :size="32" /><span class="text-xs">Profesores</span></span
         >
         <span
-          class="flex-col gap-2 flex w-20 h-32 hover:bg-emerald-200 active:bg-emerald-300 items-center justify-center"
+          class="flex-col gap-2 flex w-20 h-20 hover:bg-emerald-200 active:bg-emerald-300 items-center justify-center"
           ><menu-icon :size="32" /><span class="text-xs">Clientes</span></span
         >
         <span
-          class="flex-col gap-2 flex w-20 h-32 hover:bg-emerald-200 active:bg-emerald-300 items-center justify-center"
+          class="flex-col gap-2 flex w-20 h-20 hover:bg-emerald-200 active:bg-emerald-300 items-center justify-center"
           ><menu-icon :size="32" /><span class="text-xs">Canchas</span></span
         >
       </div>
